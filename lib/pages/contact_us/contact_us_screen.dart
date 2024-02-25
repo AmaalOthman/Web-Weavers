676d0d6c63047524685b9edff008cab6c02a9c0a
AmaalOthman <engamaal180@gmail.com>
2024-02-25 13:18:21 +0200

    + finish the ui of contact_us_screen + finish from ui of fqa_screen

--- lib/pages/contact_us/contact_us_screen.dart
+++ lib/pages/contact_us/contact_us_screen.dart
@@ -1,25 +1,136 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class ContactUsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          FadeInLeftBig(
-            child: Container(
-                child: Text(
-              'Contact Us',
-              style: Theme.of(context).textTheme.displayLarge,
-            )),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        FadeInLeftBig(
+          child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                'Contact Us',
+                style: Theme.of(context).textTheme.displayLarge,
+              )),
+        ),
+        SizedBox(
+          height: 5.h,
+        ),
+        Container(
+          margin: EdgeInsets.all(16),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Ask how we can help you',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.sp,
+                          color: Color(0xFF365486),
+                          fontFamily: GoogleFonts.acme().fontFamily,
+                        )),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Pleas note: all fieldes are required',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.sp,
+                          color: Color(0xFF365486),
+                          fontFamily: GoogleFonts.acme().fontFamily,
+                        )),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                        cursorColor: Color(0xFF365486),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            labelText: 'First Name',
+                            labelStyle: TextStyle(
+                                fontSize: 12.sp,
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.acme().fontFamily))),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                        cursorColor: Color(0xFF365486),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            labelText: 'Last Name',
+                            labelStyle: TextStyle(
+                                fontSize: 12.sp,
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.acme().fontFamily))),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                        cursorColor: Color(0xFF365486),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            labelText: 'Company Name',
+                            labelStyle: TextStyle(
+                                fontSize: 12.sp,
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.acme().fontFamily))),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                        cursorColor: Color(0xFF365486),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            labelText:
+                                'What type of solution are you looking for?',
+                            labelStyle: TextStyle(
+                                fontSize: 12.sp,
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.acme().fontFamily))),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    TextFormField(
+                        cursorColor: Color(0xFF365486),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            labelText:
+                                'Text box for the client to provide a description',
+                            labelStyle: TextStyle(
+                                fontSize: 12.sp,
+                                color: Colors.black,
+                                fontFamily: GoogleFonts.acme().fontFamily))),
+                  ],
+                ),
+              )
+            ],
           ),
-          SizedBox(
-            height: 5.h,
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
--- lib/pages/fqa/fqa_screen.dart
+++ lib/pages/fqa/fqa_screen.dart
@@ -1,46 +1,83 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_faq/flutter_faq.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class FAQScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         FadeInLeftBig(
           child: Container(
+              alignment: Alignment.center,
               child: Text(
-            'Frequantly Asked Question',
-            style: Theme.of(context).textTheme.displayLarge,
-          )),
+                'Frequantly Asked Question',
+                style: Theme.of(context).textTheme.displayLarge,
+              )),
         ),
         SizedBox(
           height: 5.h,
         ),
-        FAQ(
-          question: "Question",
-          answer: "data",
-          ansStyle: Theme.of(context).textTheme.displaySmall,
-          queStyle: Theme.of(context).textTheme.displayMedium,
-        ),
-        FAQ(
-          question: "Question",
-          answer: "data",
-          ansStyle: Theme.of(context).textTheme.displaySmall,
-          queStyle: Theme.of(context).textTheme.displayMedium,
-        ),
-        FAQ(
-          question: "Question",
-          answer: "data",
-          ansStyle: Theme.of(context).textTheme.displaySmall,
-          queStyle: Theme.of(context).textTheme.displayMedium,
-        ),
-        FAQ(
-          question: "Question",
-          answer: "data",
-          ansStyle: Theme.of(context).textTheme.displaySmall,
-          queStyle: Theme.of(context).textTheme.displayMedium,
+        Text(
+            'Below youll find answers to the question we get asked the most about applying for TextTextTextText',
+            style: TextStyle(
+                fontSize: 18.sp,
+                fontFamily: GoogleFonts.acme().fontFamily,
+                fontWeight: FontWeight.normal,
+                color: Colors.black)),
+        Container(
+          margin: EdgeInsets.all(16),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+            SizedBox(
+              height: 2.h,
+            ),
+            Container(
+              width: 50.w,
+              child: Column(
+                children: [
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  FAQ(
+                    question: "Question",
+                    answer: "data",
+                    ansStyle: Theme.of(context).textTheme.displaySmall,
+                    queStyle: Theme.of(context).textTheme.displayMedium,
+                  ),
+                ],
+              ),
+            ),
+          ]),
         ),
       ],
     );
